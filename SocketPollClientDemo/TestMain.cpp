1383367a3ca1a9ce1e27ac737fc995a37ec77f9c
pix <tpxsky@163.com>
2017-03-08 18:17:08 +0800

    add poll client test

--- SocketPollClientDemo/TestMain.cpp
+++ SocketPollClientDemo/TestMain.cpp
@@ -8,8 +8,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "test_poll_client1.h"
 
 int main(void) {
 	puts("Hello World!!!");
+	test_poll_client1();
 	return EXIT_SUCCESS;
 }
---
+++ SocketPollClientDemo/test_poll_client1.h
@@ -0,0 +1,13 @@
+/*
+ * test_poll_client1.h
+ *
+ *  Created on: 2017年3月8日
+ *      Author: Administrator
+ */
+
+#ifndef TEST_POLL_CLIENT1_H_
+#define TEST_POLL_CLIENT1_H_
+
+void test_poll_client1(void);
+
+#endif /* TEST_POLL_CLIENT1_H_ */
