4d5954e8c9946f51c924ae640da0e9ab66f9d510
pix <tpxsky@163.com>
2017-03-08 18:09:16 +0800

    add poll server test

---
+++ SocketPollServerDemo/TestMain.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : SocketPollServerDemo.cpp
+// Author      : pix
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "test_poll_server1.h"
+
+int main(void) {
+	puts("Hello World!!!");
+	test_poll_server1();
+	return EXIT_SUCCESS;
+}
